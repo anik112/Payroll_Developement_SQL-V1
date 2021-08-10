6f6da55056992651b487dc2f885cb499f5b202e6
anik112 <paulanik112@gmail.com>
2021-08-10 10:00:55 +0600

    Add new work.

--- Monthly_Payslip_wooree.sql
+++ Monthly_Payslip_wooree.sql
@@ -1,21 +1,21 @@
-SELECT sal.DEPT_BANGLA, sal.SEC_BANGLA, sal.LINENO, sal.FLOORNO, sal.MACHINENO, per.CARDNO, per.ENMNAME_BANGLA, per.JOINING_DATE, 
-       per.RESIGNATION_DATE, sal.DESIGNATION_BAN, sal.SALARY_GRD, mon.TOTAL_DAY, mon.TOTAL_LEAVE, mon.TOTAL_PRESENT, mon.TOTAL_ABSENT, 
-       mon.LATE_DAY, sal.MEDICAL, sal.TRANSPORT, sal.FOODALLOW, sal.OTHERALLOW_BK, mon.SAL_OT_HR, sal.OTRATE, mon.TOTAL_HOLIDAY,
-       (sal.GROSSSAL_CH + sal.GROSSSAL_BK)GROSSSAL, 
-       (sal.BASICSAL_CH + sal.BASICSAL_BK)BASICSAL, 
-       (sal.HOUSERENT_CH + sal.HOUSERENT_BK)HOUSERENT,
-       (sal.TOTAL_ABSENT_DED_CH + sal.TOTAL_ABSENT_DED_BK)TOTAL_ABSENT_DED,
-       (sal.NETWAGES_CH + sal.NETWAGES_BK) NETWAGES, -- check
-       (sal.SAL_OT_AMT_CH + sal.SAL_OT_AMT_BK)SAL_OT_AMT,
-       (sal.ATTBONUS_CH + sal.ATTBONUS_BK)ATTBONUS,
-       (sal.ADVANCE_AMT_CH + sal.ADVANCE_AMT_BK + sal.ADV_SAL_CH + sal.ADV_SAL_BK)ADVANCE,
-       (sal.TOTAL_DED_CH + sal.TOTAL_DED_BK)TOTAL_DEDUCTION,
-       (sal.ARREAR_AMT_CH + sal.ARREAR_AMT_BK)ARREAR_AMT,
-       (sal.NETPAYMENT_CH_RD + sal.NETPAYMENT_BK_RD)NETPAYMENT_RD,
-       (sal.NT_BILL_CH + sal.NT_BILL_PT_CH + sal.NT_BILL_BK + sal.NT_BILL_PT_BK) NT_BILL,  
-       (sal.TIFFIN_BILL_CH + sal.TIFFIN_BILL_PT_CH + sal.TIFFIN_BILL_BK + sal.TIFFIN_BILL_PT_BK) TIFFIN_BILL,
-       (sal.GT_HOLI_BILL_CH + sal.GT_HOLI_BILL_BK)GT_HOLI_BILL, 
-       (sal.WK_HOLI_BILL_BK + sal.WK_HOLI_BILL_BK)WK_HOLI_BILL, SIGN.EMPSIGN
+SELECT sal.dept_bangla, sal.sec_bangla, sal.lineno, sal.floorno, sal.machineno, per.cardno, per.enmname_bangla, per.joining_date, 
+       per.resignation_date, sal.designation_ban, sal.salary_grd, mon.total_day, mon.total_leave, mon.total_present, mon.total_absent, 
+       mon.late_day, sal.medical, sal.transport, sal.foodallow, sal.otherallow_bk, mon.sal_ot_hr, sal.otrate, mon.total_holiday,
+       (sal.grosssal_ch + sal.grosssal_bk)grosssal, 
+       (sal.basicsal_ch + sal.basicsal_bk)basicsal, 
+       (sal.houserent_ch + sal.houserent_bk)houserent,
+       (sal.total_absent_ded_ch + sal.total_absent_ded_bk)total_absent_ded,
+       (sal.netwages_ch + sal.netwages_bk) netwages, -- check
+       (sal.sal_ot_amt_ch + sal.sal_ot_amt_bk)sal_ot_amt,
+       (sal.attbonus_ch + sal.attbonus_bk)attbonus,
+       (sal.advance_amt_ch + sal.advance_amt_bk + sal.adv_sal_ch + sal.adv_sal_bk)advance,
+       (sal.total_ded_ch + sal.total_ded_bk)total_deduction,
+       (sal.arrear_amt_ch + sal.arrear_amt_bk)arrear_amt,
+       (sal.netpayment_ch_rd + sal.netpayment_bk_rd)netpayment_rd,
+       (sal.nt_bill_ch + sal.nt_bill_pt_ch + sal.nt_bill_bk + sal.nt_bill_pt_bk) nt_bill,  
+       (sal.tiffin_bill_ch + sal.tiffin_bill_pt_ch + sal.tiffin_bill_bk + sal.tiffin_bill_pt_bk) tiffin_bill,
+       (sal.gt_holi_bill_ch + sal.gt_holi_bill_bk)gt_holi_bill, 
+       (sal.wk_holi_bill_bk + sal.wk_holi_bill_bk)wk_holi_bill, SIGN.empsign
 FROM  TB_MONTH_SALARY sal,TB_MONTH_TOTALDAY  mon, TB_PERSONAL_INFO per, TB_PERSONAL_INFO_SIGN SIGN
 WHERE mon.company  = :p_company 
 AND   mon.company      = sal.company 
@@ -25,23 +25,75 @@ AND   mon.finyear          = :p_year
 AND   mon.finmonth       = :p_month 
 AND   mon.finyear          = sal.finyear 
 AND   mon.finmonth       = sal.finmonth 
-AND   (sal.NETPAYMENT_CH_RD + sal.NETPAYMENT_BK_RD) > 0 
+AND   (sal.netpayment_ch_rd + sal.netpayment_bk_rd) > 0 
 AND   mon.cardno         = sal.cardno 
 AND   mon.cardno         = per.cardno 
 AND   mon.cardno         = SIGN.cardno
-AND   per.DEPARTMENTNM LIKE DECODE(NVL(:p_dept,'all'),'all','%',:p_dept)
-AND   per.SECTIONNM          LIKE DECODE(NVL(:p_section,'all'),'all','%',:p_section)
-AND   per.DESIGNATION       LIKE DECODE(NVL(:p_designation,'all'),'all','%',:p_designation)
-AND   per.WORKERTYPE      LIKE DECODE(NVL(:p_worker,'all'),'all','%',:p_worker)
-AND   per.FLOORNO              LIKE DECODE(NVL(:p_floorno,'all'),'all','%',:p_floorno)
-AND   per.MACHINENO          LIKE DECODE(NVL(:p_machineno,'all'),'all','%',:p_machineno)	
+AND   per.departmentnm LIKE DECODE(NVL(:p_dept,'all'),'all','%',:p_dept)
+AND   per.sectionnm          LIKE DECODE(NVL(:p_section,'all'),'all','%',:p_section)
+AND   per.designation       LIKE DECODE(NVL(:p_designation,'all'),'all','%',:p_designation)
+AND   per.workertype      LIKE DECODE(NVL(:p_worker,'all'),'all','%',:p_worker)
+AND   per.floorno              LIKE DECODE(NVL(:p_floorno,'all'),'all','%',:p_floorno)
+AND   per.machineno          LIKE DECODE(NVL(:p_machineno,'all'),'all','%',:p_machineno)	
 AND   per.lineno                      LIKE DECODE(NVL(:p_line,'all'),'all','%',:p_line)
-AND   per.SHIFT                     LIKE DECODE(NVL(:p_shift,'all'),'all','%',:p_shift) 
-AND   sal.SAL_STATUS         LIKE DECODE(NVL(:p_sal_status,'all'),'all','%',:p_sal_status)
-AND   sal.PAY_STATUS        LIKE DECODE(NVL(:p_pay_status,'all'),'all','%',:p_pay_status)  
-AND   per.CASH_TYPE          LIKE DECODE(NVL(:p_payment_type,'all'),'all','%',:p_payment_type) 
-AND   per.BANK_NAME         LIKE DECODE(NVL(:p_bkname,'all'),'all','%',:p_bkname)
-AND   per.MFT_TYPE            LIKE DECODE(NVL(:p_mft_type,'all'),'all','%',:p_mft_type)
+AND   per.shift                     LIKE DECODE(NVL(:p_shift,'all'),'all','%',:p_shift) 
+AND   sal.sal_status         LIKE DECODE(NVL(:p_sal_status,'all'),'all','%',:p_sal_status)
+AND   sal.pay_status        LIKE DECODE(NVL(:p_pay_status,'all'),'all','%',:p_pay_status)  
+AND   per.cash_type          LIKE DECODE(NVL(:p_payment_type,'all'),'all','%',:p_payment_type) 
+AND   per.bank_name         LIKE DECODE(NVL(:p_bkname,'all'),'all','%',:p_bkname)
+AND   per.mft_type            LIKE DECODE(NVL(:p_mft_type,'all'),'all','%',:p_mft_type)
 AND   per.cardno                    LIKE DECODE(NVL(:p_cardno,'all'),'all','%',:p_cardno) 
 AND   per.active                     LIKE DECODE(NVL(:p_active,'all'),'all','%',:p_active)
-ORDER BY per.DEPARTMENTNM,per.SECTIONNM,per.lineno,per.cardno ASC
+ORDER BY per.departmentnm,per.sectionnm,per.lineno,per.cardno ASC
+
+
+--- total  
+
+SELECT sal.dept_bangla, sal.sec_bangla, sal.lineno, sal.floorno, sal.machineno, per.cardno, per.enmname_bangla, per.joining_date, 
+       per.resignation_date, sal.designation_ban, sal.salary_grd, mon.total_day, mon.total_leave, mon.total_present, mon.total_absent, 
+       mon.late_day, sal.medical, sal.transport, sal.foodallow, sal.otherallow_bk, mon.sal_ot_hr, sal.otrate, mon.total_holiday,
+       (sal.grosssal_ch + sal.grosssal_bk)grosssal, 
+       (sal.basicsal_ch + sal.basicsal_bk)basicsal, 
+       (sal.houserent_ch + sal.houserent_bk)houserent,
+       (sal.total_absent_ded_ch + sal.total_absent_ded_bk)total_absent_ded,
+       (sal.netwages_ch + sal.netwages_bk) netwages, -- check
+       (sal.sal_ot_amt_ch + sal.sal_ot_amt_bk)sal_ot_amt,
+       (sal.attbonus_ch + sal.attbonus_bk)attbonus,
+       (sal.advance_amt_ch + sal.advance_amt_bk + sal.adv_sal_ch + sal.adv_sal_bk)advance,
+       (sal.total_ded_ch + sal.total_ded_bk)total_deduction,
+       (sal.arrear_amt_ch + sal.arrear_amt_bk)arrear_amt,
+       (sal.netpayment_ch_rd + sal.netpayment_bk_rd)netpayment_rd,
+       (sal.nt_bill_ch + sal.nt_bill_pt_ch + sal.nt_bill_bk + sal.nt_bill_pt_bk) nt_bill,  
+       (sal.tiffin_bill_ch + sal.tiffin_bill_pt_ch + sal.tiffin_bill_bk + sal.tiffin_bill_pt_bk) tiffin_bill,
+       (sal.gt_holi_bill_ch + sal.gt_holi_bill_bk)gt_holi_bill, 
+       (sal.wk_holi_bill_bk + sal.wk_holi_bill_bk)wk_holi_bill, SIGN.empsign,5rr
+	   mon.total_work_day, mon.casual, mon.medical, mon.week_day, mon.govt_day, (sal.stamp_bk+sal.stamp_ch) stamp
+FROM  TB_MONTH_SALARY sal,TB_MONTH_TOTALDAY  mon, TB_PERSONAL_INFO per, TB_PERSONAL_INFO_SIGN SIGN
+WHERE mon.company  = :p_company 
+AND   mon.company      = sal.company 
+AND   mon.company      = per.company 
+AND   mon.company      = SIGN.company
+AND   mon.finyear          = :p_year 
+AND   mon.finmonth       = :p_month 
+AND   mon.finyear          = sal.finyear 
+AND   mon.finmonth       = sal.finmonth 
+AND   (sal.netpayment_ch_rd + sal.netpayment_bk_rd) > 0 
+AND   mon.cardno         = sal.cardno 
+AND   mon.cardno         = per.cardno 
+AND   mon.cardno         = SIGN.cardno
+AND   per.departmentnm LIKE DECODE(NVL(:p_dept,'all'),'all','%',:p_dept)
+AND   per.sectionnm          LIKE DECODE(NVL(:p_section,'all'),'all','%',:p_section)
+AND   per.designation       LIKE DECODE(NVL(:p_designation,'all'),'all','%',:p_designation)
+AND   per.workertype      LIKE DECODE(NVL(:p_worker,'all'),'all','%',:p_worker)
+AND   per.floorno              LIKE DECODE(NVL(:p_floorno,'all'),'all','%',:p_floorno)
+AND   per.machineno          LIKE DECODE(NVL(:p_machineno,'all'),'all','%',:p_machineno)	
+AND   per.lineno                      LIKE DECODE(NVL(:p_line,'all'),'all','%',:p_line)
+AND   per.shift                     LIKE DECODE(NVL(:p_shift,'all'),'all','%',:p_shift) 
+AND   sal.sal_status         LIKE DECODE(NVL(:p_sal_status,'all'),'all','%',:p_sal_status)
+AND   sal.pay_status        LIKE DECODE(NVL(:p_pay_status,'all'),'all','%',:p_pay_status)  
+AND   per.cash_type          LIKE DECODE(NVL(:p_payment_type,'all'),'all','%',:p_payment_type) 
+AND   per.bank_name         LIKE DECODE(NVL(:p_bkname,'all'),'all','%',:p_bkname)
+AND   per.mft_type            LIKE DECODE(NVL(:p_mft_type,'all'),'all','%',:p_mft_type)
+AND   per.cardno                    LIKE DECODE(NVL(:p_cardno,'all'),'all','%',:p_cardno) 
+AND   per.active                     LIKE DECODE(NVL(:p_active,'all'),'all','%',:p_active)
+ORDER BY per.departmentnm,per.sectionnm,per.lineno,per.cardno ASC
